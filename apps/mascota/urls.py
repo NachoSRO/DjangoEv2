18b7c6a2db85879624e7cbe1e8964608e819e62f
Ignacio Sepúlveda <ign.spr@live.com>
2018-11-17 00:27:40 -0300

    Edicion HTML / CSS

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('nuevo', views.MascotaCreate.as_view(), name='mascota_crear'),
     path('listar', views.MascotaList.as_view(), name='mascota listar'),
     path('editar/<pk>/', views.MascotaUpdate.as_view(), name='mascota_editar'),
-    path('eliminar/<pk>/', views.MascotaDelete.as_view(), name='mascota_eliminar')
-
+    path('eliminar/<pk>/', views.MascotaDelete.as_view(), name='mascota_eliminar'),
 ]
